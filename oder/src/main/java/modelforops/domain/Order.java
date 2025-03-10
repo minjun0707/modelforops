63d8fff663d82a6a687bf317bb055f464b7d9fa9
김민준 <75360915+minjun0707@users.noreply.github.com>
2025-03-10 10:06:26 +0900

    commit

---
+++ oder/src/main/java/modelforops/domain/Order.java
@@ -0,0 +1,35 @@
+package modelforops.domain;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import javax.persistence.*;
+import lombok.Data;
+import modelforops.OderApplication;
+
+@Entity
+@Table(name = "Order_table")
+@Data
+//<<< DDD / Aggregate Root
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String productName;
+
+    private Integer qty;
+
+    private String status;
+
+    public static OrderRepository repository() {
+        OrderRepository orderRepository = OderApplication.applicationContext.getBean(
+            OrderRepository.class
+        );
+        return orderRepository;
+    }
+}
+//>>> DDD / Aggregate Root
